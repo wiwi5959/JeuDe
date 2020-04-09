916be147c219bc8f67601fbaa755548b14c529a0
wiwi5959 <wiwi5959@outlook.fr>
2020-04-09 17:00:42 +0200

    wip

---
+++ JeuDe/JeuDe/Class4/Partie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JeuDe.Class3;
+using JeuDe.Class2;
+
+namespace JeuDe.Class4
+{
+    static class Partie
+    {
+        public static Joueur joueur;
+        public static Gobelets gobelets;
+        public static De de;
+        static Joueur[] participant= new Joueur[4];
+
+
+
+
+    }
+}
---
+++ JeuDe/JeuDe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JeuDe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
